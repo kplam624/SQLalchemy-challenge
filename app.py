54f8a53ddc7327164060b1331b7787d523d4308c
kplam624 <kplam624@gmail.com>
2020-11-11 13:57:15 -0500

    Completed Precipitation api.

--- app.py
+++ app.py
@@ -31,6 +31,28 @@ def home():
         f"/api/v1.0/start/end<br/>"
     )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+    sel = {meas.date, func.avg(meas.prcp)}
+
+    prcp_val = session.query(*sel).\
+        filter(meas.date > '2016-08-23').\
+        group_by(meas.date).\
+        order_by(meas.date).all()
+    
+    session.close()
+
+    prcp = []
+    for date, average in prcp_val:
+        prcp_dict = {}
+        prcp_dict['Date'] = date
+        prcp_dict['Average Precipitation'] = average
+        prcp.append(prcp_dict)
+    
+    return jsonify(prcp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
