d25c77e380b425695578674ab8874692f542f1c2
kplam624 <kplam624@gmail.com>
2020-11-14 21:52:55 -0500

    Commented the code.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# Imported dependancies
+
 import numpy as np
 
 import sqlalchemy
@@ -5,23 +7,31 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+# Importing flask and jsonify for api creation.
 from flask import Flask, jsonify
 
+# Creating a path to the sql database.
 climate_path = "Resources/hawaii.sqlite"
+
+# Creating an engine that connects to the database.
 engine = create_engine(f'sqlite:///{climate_path}')
 
+# Creates a base to reflect the tables to create classes.
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
+# Creates the classes that are reflected by the tables.
 meas = Base.classes.measurement
 sta = Base.classes.station
 
+# Starting the app.
 app = Flask(__name__)
 
+# Home route.
 @app.route("/")
 def home():
     """Homepage for the api"""
-
+    # Shows the routes.
     return (
         f"Welcome! Here are the following api routes.<br/>"
         f"/api/v1.0/precipitation<br/>"
@@ -33,19 +43,26 @@ def home():
         f"/api/v1.0/help"
     )
 
+# Route for Precipitation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+
+    # Create session connecting to the engine.
     session = Session(engine)
 
+    # Query values
     sel = {meas.date, func.avg(meas.prcp)}
 
+    # Session Query
     prcp_val = session.query(*sel).\
         filter(meas.date > '2016-08-23').\
         group_by(meas.date).\
         order_by(meas.date).all()
-    
+
+    # Closing the session
     session.close()
 
+    # Empty list to add the dictionary values to.
     prcp = []
     for date, average in prcp_val:
         prcp_dict = {}
@@ -53,35 +70,46 @@ def precipitation():
         prcp_dict['Average Precipitation'] = average
         prcp.append(prcp_dict)
     
+    # jsonify the results
     return jsonify(prcp)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    session = Session(engine)
 
+     # Create session connecting to the engine.
+    session = Session(engine)
+    
+    # Session Query
     results = session.query(sta.station).all()
 
+    # Closing the session
     session.close()
 
+    # Empty list to add the dictionary values to.
     station = []
     for name in results:
         station_dict = {}
         station_dict['Name'] = name
         station.append(station_dict)
     
+    # jsonify the results
     return jsonify(station)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+     # Create session connecting to the engine.
     session = Session(engine)
 
+    # Session Query
     sel3 = (meas.date, meas.tobs)
     station_tobs = session.query(*sel3).filter(meas.date > '2016-08-23',meas.station == 'USC00519281').\
         group_by(meas.date).\
         order_by(meas.date).all()
-    
+
+    # Closing the session
     session.close()
 
+    # Empty list to add the dictionary values to.
     tob = []
     for date, temp in station_tobs:
         stat_tobs={}
@@ -89,15 +117,22 @@ def tobs():
         stat_tobs['Temperature'] = temp
         tob.append(stat_tobs)
 
+    # jsonify the results
     return jsonify(tob)
 
 @app.route("/api/v1.0/<start>")
 def temp(start):
+     # Create session connecting to the engine.
     session = Session(engine)
+
+    # Session Query
     temperature = session.query(func.min(meas.tobs),func.max(meas.tobs),func.avg(meas.tobs)).\
         filter(meas.date >= start).all()
+
+    # Closing the session
     session.close()
 
+    # Empty list to add the dictionary values to.
     temp_imp = []
     for min,max,avg in temperature:
         temp_dict = {}
@@ -105,20 +140,29 @@ def temp(start):
         temp_dict['Max'] = max
         temp_dict['Average'] = avg
         temp_imp.append(temp_dict)
-    
+
+    # Error message    
     if temp_imp[0]['Min'] is None:
         return "Oops. Something went wrong. Is your date in the YYYY-MM-DD format?<br/>" \
             "Please try again or go to /api/v1.0/help"
 
+    # jsonify the results
     return jsonify(temp_imp) 
 
 @app.route("/api/v1.0/<start>/<end>")
 def temps(start, end):
+
+     # Create session connecting to the engine.
     session = Session(engine)
+
+    # Session Query
     temperatures = session.query(func.min(meas.tobs),func.max(meas.tobs),func.avg(meas.tobs)).\
         filter(meas.date >= start,meas.date <= end).all()
+
+    # Closing the session
     session.close()
 
+    # Empty list to add the dictionary values to.
     temp_s_e = []
     for min,max,avg in temperatures:
         s_e_dict = {}
@@ -127,14 +171,17 @@ def temps(start, end):
         s_e_dict['Average'] = avg
         temp_s_e.append(s_e_dict)
 
+    # Error message
     if temp_s_e[0]['Min'] is None:
         return "Oops. Something went wrong. Is your date in the YYYY-MM-DD format?<br/>" \
             "Please try again or go to /api/v1.0/help"
 
+    # jsonify the results
     return jsonify(temp_s_e)    
 
 @app.route("/api/v1.0/help")
 def help():
+    # Creating a route that holds information on the api.
     return f"Hi, this is a quick rundown on what each route does. <br/>"\
         "<br/> The api will pull from a database that contains the climate data from Hawaii (currently upto 2017). <br/>"\
         "/api/v1.0/precipitation shows the precipitation shows the precipitation data for the past year (2016-2017).<br/>"\
@@ -144,6 +191,6 @@ def help():
         "/api/v1.0/start/end shows the min, max and average temperature between the requested dates (Up to 2017-08-23). Replace 'start' and 'end' with a date in YYYY-MM-DD format."\
 
 
-
+# Last line of the api. Debugger.
 if __name__ == '__main__':
     app.run(debug=True)
